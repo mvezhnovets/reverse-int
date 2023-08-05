d92db2ac5053ba43e1da9223d9cf403db29a4441
Mikhail Vezhnovets <mvezhnovets@gmail.com>
2023-08-05 13:32:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+
+  const string = String(n);
+
+  let b = string[0] === '-' ? string.slice(1) : string;
+
+
+  let reverseString = b.split('').reverse();
+
+  reverseString[0] === '0' ? string.slice(1) : reverseString;
+
+  let a = reverseString.join('');
+
+  return Number(a)
 }
